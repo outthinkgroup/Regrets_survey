49e4f0c71458b9cf21445baea5bc93ad5f2f77c0
Joshua Kennedy <josh@outthinkgroup.com>
2020-04-01 14:20:56 -0400

    added world map

--- src/components/RegretsMap.js
+++ src/components/RegretsMap.js
@@ -4,8 +4,8 @@ import "./Map.css"
 import Map from "./Map"
 import { useState, useEffect } from "react"
 
-const WIDTH = 959
-const HEIGHT = 593
+const WIDTH = 1009.6727
+const HEIGHT = 665.96301
 export default function RegretsMap() {
   const svgRef = useRef()
   const [zoomed, setZoomed] = useState(false)
@@ -46,65 +46,72 @@ export default function RegretsMap() {
   const focusOnState = e => {
     const state = e.target.getBoundingClientRect()
     const svg = e.target.parentElement.parentElement.getBoundingClientRect()
-
     const stateRelative = {
       width: (state.width / svg.width) * WIDTH,
       height: (state.height / svg.height) * HEIGHT,
       x: ((state.left - svg.left) / svg.width) * WIDTH,
       y: ((state.top - svg.top) / svg.height) * HEIGHT,
     }
+    if (!zoomed) {
+      if (!e.target.id || e.target.id === "DC2") return
+      e.target.dataset.active = "true"
+      //sets it to be half the size of the viewport
+      const twiceStateWidth = stateRelative.width * 2.5
+      const twiceStateHeight = getRatio(
+        stateRelative.height,
+        stateRelative.width,
+        twiceStateWidth
+      )
 
-    //sets it to be half the size of the viewport
-    const twiceStateWidth = stateRelative.width * 2
-    const twiceStateHeight = getRatio(
-      stateRelative.height,
-      stateRelative.width,
-      twiceStateWidth * 0.8
-    )
-
-    const orientation = getOrientation(
-      width,
-      height,
-      twiceStateWidth,
-      twiceStateHeight
-    )
-    const heightAndWidthViewPort = () => {
-      if (orientation === width) {
-        const widthViewPort = twiceStateWidth
-        const heightViewPort = getRatio(height, width, twiceStateWidth)
-        return { widthViewPort, heightViewPort }
-      } else {
-        const heightViewPort = twiceStateHeight
-        const widthViewPort = getRatio(width, height, twiceStateHeight)
-        return { widthViewPort, heightViewPort }
+      const orientation = getOrientation(
+        width,
+        height,
+        twiceStateWidth,
+        twiceStateHeight
+      )
+      const heightAndWidthViewPort = () => {
+        if (orientation === width) {
+          const widthViewPort = twiceStateWidth
+          const heightViewPort = getRatio(height, width, twiceStateWidth)
+          return { widthViewPort, heightViewPort }
+        } else {
+          const heightViewPort = twiceStateHeight
+          const widthViewPort = getRatio(width, height, twiceStateHeight)
+          return { widthViewPort, heightViewPort }
+        }
       }
+
+      const { widthViewPort, heightViewPort } = heightAndWidthViewPort()
+      const distanceWidth = width - widthViewPort
+      const distanceHeight = height - heightViewPort
+      const distanceXPos =
+        stateRelative.x - xPos - widthViewPort + twiceStateWidth / 2 + 15
+      const distanceYPos =
+        stateRelative.y - yPos - heightViewPort + twiceStateHeight * 0.75
+
+      const viewPort = {
+        distanceXPos,
+        distanceYPos,
+        distanceWidth,
+        distanceHeight,
+      }
+
+      animateViewBoxScale(
+        viewPort.distanceXPos,
+        viewPort.distanceYPos,
+        viewPort.distanceWidth,
+        viewPort.distanceHeight
+      )
+    } else {
+      animateViewBoxScale(xPos * -1, yPos * -1, width - WIDTH, height - HEIGHT)
+      delete document.querySelector("[data-active='true']").dataset.active
     }
-
-    const { widthViewPort, heightViewPort } = heightAndWidthViewPort()
-    const distanceWidth = width - widthViewPort
-    const distanceHeight = height - heightViewPort
-    const distanceXPos =
-      stateRelative.x - xPos - widthViewPort + twiceStateWidth / 2
-    const distanceYPos =
-      stateRelative.y - yPos - heightViewPort + twiceStateHeight * 0.75
-
-    const viewPort = {
-      distanceXPos,
-      distanceYPos,
-      distanceWidth,
-      distanceHeight,
-    }
-
-    animateViewBoxScale(
-      viewPort.distanceXPos,
-      viewPort.distanceYPos,
-      viewPort.distanceWidth,
-      viewPort.distanceHeight
-    )
+    setZoomed(!zoomed)
   }
 
   return (
     <div
+      className={zoomed && `zoomed`}
       onClick={focusOnState}
       style={{ position: "relative", width: `100%`, height: `100%` }}
     >
