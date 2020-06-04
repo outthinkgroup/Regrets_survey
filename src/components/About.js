3d292c5ca7df4586d5712a92beecaa577af6dc86
Joshua Kennedy <josh@outthinkgroup.com>
2020-06-04 11:51:02 -0400

    minor styles to the author section

--- src/components/About.js
+++ src/components/About.js
@@ -3,18 +3,32 @@ import styled from "styled-components";
 
 import AuthorImage from "./AuthorImage";
 import { AboutContent } from "./content";
+import { breakpoints } from "../styles";
 function About({ className }) {
   return (
     <div className={className}>
       <AuthorImage />
-      <h2>About the Author</h2>
-      <AboutContent />
+      <div className="content">
+        <h2>About the Author</h2>
+        <AboutContent />
+      </div>
     </div>
   );
 }
 export default styled(About)`
-  text-align: center;
-  h2 {
-    margin: 20px;
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: ${breakpoints.small}) {
+    display: flex;
+    align-items: center;
+    .content {
+      margin-left: 50px;
+      p {
+        margin-bottom: 0;
+      }
+    }
+  }
+  .author-image {
+    min-width: 125px;
   }
 `;
