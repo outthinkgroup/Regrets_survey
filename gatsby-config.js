069949bed24b8f848329b47c7640d13b0213a3d9
Joshua Kennedy <josh@outthinkgroup.com>
2020-06-08 12:17:10 -0400

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,8 +64,8 @@ For a book about regret, author Daniel H. Pink is collecting regrets from all ov
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `World Regret Survey`,
+        short_name: `World Regret Survey`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -76,5 +76,24 @@ For a book about regret, author Daniel H. Pink is collecting regrets from all ov
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: `UA-5156258-2`,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+
+        defer: false,
+      },
+    },
   ],
 };
