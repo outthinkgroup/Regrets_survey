46c93e37254506dc3406b3306dd1c3dd1cde6526
Joshua Kennedy <josh@outthinkgroup.com>
2020-06-09 13:04:33 -0400

    actually returning stuf

--- functions/triggerRebuild.js
+++ functions/triggerRebuild.js
@@ -12,7 +12,7 @@ exports.handler = async function(event, context) {
     });
     console.log(response);
   }
-  triggerDeploy("outthinkgroup", "Regrets_survey")
+  return triggerDeploy("outthinkgroup", "Regrets_survey")
     .then((res) => ({
       statusCode: 200,
       body: JSON.stringify({
