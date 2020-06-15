e43f803e1979203e6b284b5215ea2c6ae546c75f
Joshua Kennedy <josh@outthinkgroup.com>
2020-06-15 12:50:58 -0400

    removed typekit

--- src/html.js
+++ src/html.js
@@ -11,10 +11,6 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link
-          rel="stylesheet"
-          href="https://use.typekit.net/zdm3twp.css"
-        ></link>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
