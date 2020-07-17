2c14071eb25cd4c5397042cea31dcb73e759027b
Joshua Kennedy <josh@outthinkgroup.com>
2020-07-17 14:00:32 -0400

    added webhook

--- githubAction/index.js
+++ githubAction/index.js
@@ -1,1 +1,13 @@
-console.log("object");
+const fetch = require("node-fetch");
+
+async function triggerBuild() {
+  const res = await fetch(
+    "https://worldregretsurvey.com//.netlify/functions/triggerRebuild",
+    {
+      method: "POST",
+      body: JSON.stringify({ branch: "master" }),
+    }
+  ).then((res) => res.json());
+  console.log(res);
+}
+triggerBuild();
