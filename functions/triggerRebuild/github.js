718ad0d86d92261172d72703719de06527ef1d7b
Joshua Kennedy <josh@outthinkgroup.com>
2020-07-17 14:44:59 -0400

    commit

--- functions/triggerRebuild/github.js
+++ functions/triggerRebuild/github.js
@@ -50,7 +50,7 @@ async function updateFileInGit({
     .catch((e) => {
       errorMessages.push({ updateFile: e });
     });
-<<<<<<< HEAD
+
   return {
     updateFileResults: res,
     dataFromQualtrics: { data },
@@ -59,10 +59,6 @@ async function updateFileInGit({
 
   //!end of function
   ///?--//0--/
-=======
-  return res;
-  //end of function
->>>>>>> commit
 
   async function getFile({ filepath }) {
     const response = await octokit.repos.getContents({
