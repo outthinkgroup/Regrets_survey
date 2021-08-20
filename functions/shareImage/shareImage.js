e264c6e71842c5665f1994031e832124a91376a2
Joshua Kennedy <josh@outthinkgroup.com>
2021-08-20 12:06:55 -0400

    function folder org

---
+++ functions/shareImage/shareImage.js
@@ -0,0 +1,11 @@
+const puppeteer = require("puppeteer-core");
+const { builder } = require("@netlify/functions");
+const { webScrape } = require("./webscrape.js");
+const chromium = require("chrome-aws-lambda");
+
+const handler = async (event, context) => {
+  res = await webScrape({ event, context }, chromium, true);
+  return res;
+};
+
+exports.handler = builder(handler);
